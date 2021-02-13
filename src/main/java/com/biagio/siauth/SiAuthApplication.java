1c27132d02eb10b3b8e30e5d522ab8c4dbebe02f
leobiagio <leobiagio@hotmail.com>
2021-02-13 15:43:43 -0300

    Initial comit

---
+++ src/main/java/com/biagio/siauth/SiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.biagio.siauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biagio/siauth/SiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biagio.siauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
